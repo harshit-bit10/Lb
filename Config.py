ab4843335eea7dc7b1133726f48369deb097296e
harshit-bit10 <109902163+harshit-bit10@users.noreply.github.com>
2025-04-01 19:35:33 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -5,14 +5,18 @@ import os
 # Bot token from @botfather
 BOT_TOKEN = ""
 # From my.telegram.org/
-API_ID = ""
-API_HASH =  ""
+API_ID = "26121146"
+API_HASH =  "985f3a097d33e1eb0e555bfd040d3ebf"
 # For /log cmd
 OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "969084369").split(" ")]
 # No time limit for this users
-AUTH_USERS = [969084369, 2800583882]
+AUTH_USERS = [6066102279]
 # Time gap after each request (in seconds) 
 TIME_GAP = 0
-BOT_USERNAME='test' #bot username without@
+BOT_USERNAME='Iptvsharkbot' #bot username without@
 API_TOKEN='b7d9e1f7-12a6-4b8e-89a2-c3d124e5f6a7' #don't change this without developer permission
 
+
+
+ 
+7240304290:
